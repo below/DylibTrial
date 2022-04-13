49d7da5be9c8fbd51d1d27563e276a1ec8614271
zoha131 <abir.zoha@nexdecade.com>
2022-04-13 21:54:34 +0600

    Initial Commit

---
+++ DylibProject/DylibProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DylibProjectApp.swift
+//  DylibProject
+//
+//  Created by MD Abir Hasan Zoha on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct DylibProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
